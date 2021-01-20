909267dfe489e0faf4f35355bd52a7d8d9e4c302
sourabh <dgr8.sourabh@gmail.com>
2021-01-20 11:17:41 +0530

    second commit

--- app1/views.py
+++ app1/views.py
@@ -4,4 +4,4 @@ from django.http import HttpResponse
 
 
 def homepage(request):
-    return HttpResponse("<h1>Welcome To django Framework</h1>")
+    return HttpResponse("<h1>Hi, django Framework is Easy to learn.</h1>")
