5f015a264f474019a4467f704a7c79563593ff68
dgr8sourabh <54842802+dgr8sourabh@users.noreply.github.com>
2021-01-20 13:13:14 +0530

    Update views.py

--- app1/views.py
+++ app1/views.py
@@ -4,4 +4,4 @@ from django.http import HttpResponse
 
 
 def homepage(request):
-    return HttpResponse("<center><h1>Welcome to django Framework.</h1>")
+    return HttpResponse("<center><h1>Welcome to django Framework.</h1></center>")
